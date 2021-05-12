b9e98c4fb82bb977f110f37d0eac121c79326a0d
ParthTagalpallewar <55437315+PArth-tagalpallewar@users.noreply.github.com>
2021-05-13 00:42:55 +0530

    HomeScrren Done

--- app/src/main/java/com/cowin/vaccinateme/ui/home/HomeFragment.kt
+++ app/src/main/java/com/cowin/vaccinateme/ui/home/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.cowin.vaccinateme.ui.home
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,23 +11,31 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.cowin.vaccinateme.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        website.setOnClickListener {
+
+            val cowinWebsite = "https://www.cowin.gov.in/home"
+            Intent(Intent.ACTION_VIEW, Uri.parse(cowinWebsite)).apply {
+                startActivity(this)
+            }
+        }
     }
 }
