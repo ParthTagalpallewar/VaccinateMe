66942704e28f69c5c4a1e7005b5bda7c59894e95
Nikhil Devshatwar <nikhil.nd@ti.com>
2021-05-13 20:32:22 +0530

    Interstitial Ad working fine

--- app/src/main/java/com/cowin/vaccinateme/ui/home/HomeFragment.kt
+++ app/src/main/java/com/cowin/vaccinateme/ui/home/HomeFragment.kt
@@ -3,18 +3,23 @@ package com.cowin.vaccinateme.ui.home
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.cowin.vaccinateme.R
 import com.cowin.vaccinateme.utils.AdsManager
-import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private var mInterstitialAd: InterstitialAd? = null
+    private final var TAG = "MainActivity"
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -28,6 +33,34 @@ class HomeFragment : Fragment() {
         val mAdView = root.findViewById<AdView>(R.id.adView)
         adsManager.createAds(mAdView)
 
+        var adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(activity,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.e(TAG, adError?.message)
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.e(TAG, "Ad was loaded.")
+                mInterstitialAd = interstitialAd
+            }
+        })
+
+        mInterstitialAd?.fullScreenContentCallback = object: FullScreenContentCallback() {
+            override fun onAdDismissedFullScreenContent() {
+                Log.d(TAG, "Ad was dismissed.")
+            }
+
+            override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
+                Log.d(TAG, "Ad failed to show.")
+            }
+
+            override fun onAdShowedFullScreenContent() {
+                Log.d(TAG, "Ad showed fullscreen content.")
+                mInterstitialAd = null;
+            }
+        }
         return root
     }
 
@@ -42,5 +75,14 @@ class HomeFragment : Fragment() {
                 startActivity(this)
             }
         }
+
+        showAdBtn.setOnClickListener() {
+            if (mInterstitialAd != null) {
+                mInterstitialAd?.show(activity)
+            } else {
+                Log.d("TAG", "The interstitial ad wasn't ready yet.")
+            }
+        }
+
     }
 }
--- app/src/main/java/com/cowin/vaccinateme/utils/AdsManager.kt
+++ app/src/main/java/com/cowin/vaccinateme/utils/AdsManager.kt
@@ -7,45 +7,7 @@ import android.widget.Toast
 import com.google.android.gms.ads.*
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener
 import com.google.android.gms.ads.interstitial.InterstitialAd
-/*
-val TAG:String = "AdsManager"
 
-class MyInterstitialAd: InterstitialAd() {
-    override fun getAdUnitId(): String {
-        return "ca-app-pub-3940256099942544/1033173712"
-    }
-
-    override fun show(p0: Activity) {
-        Log.e(TAG, "Show")
-    }
-
-    override fun setFullScreenContentCallback(p0: FullScreenContentCallback?) {
-        Log.e(TAG, "setFullscreen CB")
-    }
-
-    override fun getFullScreenContentCallback(): FullScreenContentCallback? {
-        Log.e(TAG, "setFullscreen CB")
-        return this
-    }
-
-    override fun setImmersiveMode(p0: Boolean) {
-        Log.e(TAG, "set immersive mode")
-    }
-
-    override fun getResponseInfo(): ResponseInfo {
-        Log.e(TAG, "get Response")
-    }
-
-    override fun setOnPaidEventListener(p0: OnPaidEventListener?) {
-        Log.e(TAG, "set paid event listener")
-    }
-
-    override fun getOnPaidEventListener(): OnPaidEventListener? {
-        Log.e(TAG, "get paid event listener")
-    }
-
-}
-*/
 open class AdsManager {
 
     private lateinit var context:Context
@@ -77,13 +39,6 @@ open class AdsManager {
 
 
     /*
-
-    open fun createInterstitialAds(): InterstitialAd? {
-        val adRequest = AdRequest.Builder().build()
-        interstitialAd = MyInterstitialAd()
-        return interstitialAd
-    }
-
 
     //For Industrial Ads
     open fun createInterstitialAds(): InterstitialAd? {
