503d8bf0061b3a06729fd9e163e7a1c23795f48b
ParthTagalpallewar <55437315+PArth-tagalpallewar@users.noreply.github.com>
2021-05-14 23:03:18 +0530

    Profile fragment_profile.xml

--- app/src/main/java/com/cowin/vaccinateme/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/cowin/vaccinateme/ui/profile/ProfileFragment.kt
@@ -3,9 +3,11 @@ package com.cowin.vaccinateme.ui.profile
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
@@ -14,7 +16,10 @@ import androidx.navigation.fragment.findNavController
 import com.cowin.vaccinateme.R
 import com.cowin.vaccinateme.data.repositionries.UserDataRepositories
 import com.cowin.vaccinateme.utils.AdsManager
-import com.google.android.gms.ads.AdView
+import com.cowin.vaccinateme.utils.templateAds.NativeTemplateStyle
+import com.cowin.vaccinateme.utils.templateAds.TemplateView
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.nativead.NativeAd
 import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.coroutines.flow.collect
 import java.lang.Appendable
@@ -31,9 +36,7 @@ class ProfileFragment : Fragment() {
 
         var root = inflater.inflate(R.layout.fragment_profile, container, false)
 
-        val adsManager = AdsManager(requireContext())
-        val mAdView = root.findViewById<AdView>(R.id.adViewProfile)
-        adsManager.createAds(mAdView)
+        showNativeTempAds(root)
 
         return root
     }
@@ -75,5 +78,38 @@ class ProfileFragment : Fragment() {
             }
 
         }
+    }
+
+    fun showNativeTempAds(root: View) {
+
+
+        MobileAds.initialize(requireContext()) {
+
+        }
+
+        val adLoader = AdLoader.Builder(requireContext(), "ca-app-pub-3940256099942544/2247696110")
+            .forNativeAd { ad: NativeAd ->
+                val styles = NativeTemplateStyle.Builder().build()
+
+                val template = root.findViewById<TemplateView>(R.id.my_template)
+                template.setStyles(styles)
+                template.visibility = View.VISIBLE
+                template.setNativeAd(ad)
+            }.withAdListener(object : AdListener() {
+                override fun onAdFailedToLoad(p0: LoadAdError) {
+                    super.onAdFailedToLoad(p0)
+                    Toast.makeText(requireContext(), p0.message, Toast.LENGTH_SHORT).show()
+
+                }
+
+                override fun onAdLoaded() {
+                    super.onAdLoaded()
+                    /// Toast.makeText(requireContext(),"adLodedSuccessfully", Toast.LENGTH_SHORT).show()
+                }
+            }).build()
+
+        adLoader.loadAd(AdRequest.Builder().build())
+
+
     }
 }
